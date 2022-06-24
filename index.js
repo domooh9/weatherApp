c1f7281c7a1c7063a5853a443989344b33a5a884
Domnic Omondi <domooh9@gmail.com>
2022-06-24 19:56:00 +0300

    fetching weather to the default city Nairobi

--- index.js
+++ index.js
@@ -10,34 +10,42 @@ let weather={
     .then((data) => this.displayWeather(data));
     },
     //declaring data from the API
-displayWeather: function(data)
-{
-const { name } = data;
-const { icon, description } = data.weather[0];
-const { temp, humidity } = data.main;
-const { speed } = data.wind;
-//using data from the API to manipulate the innerText of HTML
-document.querySelector(".city").innerText = "Weather of " + name;
-document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
-document.querySelector(".description").innertext = description; 
-document.querySelector(".temp").innerText = temp + "°C";
-document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-document.querySelector(".wind").innerText = "wind speed: " + speed + "km/hr";
-document.querySelector(".weather").classList.remove("loading");
-},
-//activating the search button to get data response from the server
-search: function (){
-    this.fetchWeather(document.querySelector(".search-bar").value);
-}
-};
-//adding event Listener to the search button
-document.querySelector(".search button")
-.addEventListener("click", function(){
- weather.search();
-});
-//using ENTER button to search data from the API
-document.querySelector(".search-bar").addEventListener
-("keyup", function(event){
-    if(EventTarget.key == "Enter"){
+    displayWeather: function(data)
+    {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    //using data from the API to manipulate the innerText of HTML
+    document.querySelector(".city").innerText = "Weather of " + name;
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+    document.querySelector(".description").innertext = description; 
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = "wind speed: " + speed + "km/hr";
+    document.querySelector(".weather").classList.remove("loading");
+    },
+    //activating the search button to get data response from the server
+    search: function (){
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+    };
+    //adding event Listener to the search button
+    document.querySelector(".search button")
+    .addEventListener("click", function(){
+     weather.search();
+    });
+    //using ENTER button to search data from the API
+    document.querySelector(".search-bar").addEventListener
+    ("keyup", function(event){
+        if(EventTarget.key == "Enter"){
+            weather.search();
+        }
+    });
+    document.querySelector(".search-bar")
+    .addEventListener("keyup", function (event){
+    if(event.key == "Enter"){
         weather.search();
     }
+    })
+    weather.fetchWeather("Nairobi");
